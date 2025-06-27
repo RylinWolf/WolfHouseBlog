9c73ae257b19cfc494edbda624e64d613f79ac6e
LinexSong <linexwolf@icloud.com>
2025-06-27 15:35:09 +0800

    build: 初始提交

---
+++ src/main/java/com/wolfhouse/wolfhouseblog/WolfHouseBlogApplication.java
@@ -0,0 +1,16 @@
+package com.wolfhouse.wolfhouseblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author rylinwolf
+ */
+@SpringBootApplication
+public class WolfHouseBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WolfHouseBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wolfhouse/wolfhouseblog/WolfHouseBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolfhouse.wolfhouseblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WolfHouseBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
