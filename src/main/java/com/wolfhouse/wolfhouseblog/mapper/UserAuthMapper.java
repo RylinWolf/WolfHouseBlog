fca5e4cd639949c3749c4a2498bc4b6d5cfabbc2
LinexSong <linexwolf@icloud.com>
2025-07-28 17:55:40 +0800

    feat: 新增插入用户认证记录 Mapper 方法

--- src/main/java/com/wolfhouse/wolfhouseblog/mapper/UserAuthMapper.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/mapper/UserAuthMapper.java
@@ -2,12 +2,25 @@ package com.wolfhouse.wolfhouseblog.mapper;
 
 import com.mybatisflex.core.BaseMapper;
 import com.wolfhouse.wolfhouseblog.pojo.domain.UserAuth;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 
 /**
+ * 用户认证数据访问接口
+ *
  * @author linexsong
  */
 @Mapper
 public interface UserAuthMapper extends BaseMapper<UserAuth> {
 
+    /**
+     * 插入一条用户认证记录
+     *
+     * @param auth 用户认证信息对象
+     * @return 影响的行数
+     */
+    @Insert("insert into user_auth(password) values (#{password})")
+    @Options(useGeneratedKeys = true, keyProperty = "userId")
+    Integer insertOne(UserAuth auth);
 }
--- src/main/java/com/wolfhouse/wolfhouseblog/pojo/domain/UserAuth.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/pojo/domain/UserAuth.java
@@ -3,8 +3,10 @@ package com.wolfhouse.wolfhouseblog.pojo.domain;
 import com.mybatisflex.annotation.Column;
 import com.mybatisflex.annotation.Id;
 import com.mybatisflex.annotation.Table;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -16,9 +18,12 @@ import org.springframework.stereotype.Component;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@Schema(name = "用户验证实体")
 @Table("user_auth")
 public class UserAuth {
     @Id
+    @Column(value = "id")
     private Long userId;
     @Size(min = 6, max = 20)
     private String password;
