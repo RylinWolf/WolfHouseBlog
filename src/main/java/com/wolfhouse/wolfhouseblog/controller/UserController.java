fb1b3eb4434d985340a33e09d5bedfb86dc8173d
LinexSong <linexwolf@icloud.com>
2025-07-28 18:01:53 +0800

    feat: 新增用户控制器，实现用户登陆和注册功能

---
+++ src/main/java/com/wolfhouse/wolfhouseblog/controller/UserController.java
@@ -0,0 +1,81 @@
+package com.wolfhouse.wolfhouseblog.controller;
+
+import com.wolfhouse.wolfhouseblog.common.constant.AuthExceptionConstant;
+import com.wolfhouse.wolfhouseblog.common.constant.services.UserConstant;
+import com.wolfhouse.wolfhouseblog.common.http.HttpCodeConstant;
+import com.wolfhouse.wolfhouseblog.common.http.HttpResult;
+import com.wolfhouse.wolfhouseblog.pojo.dto.UserLoginDto;
+import com.wolfhouse.wolfhouseblog.pojo.dto.UserRegisterDto;
+import com.wolfhouse.wolfhouseblog.pojo.vo.UserLoginVo;
+import com.wolfhouse.wolfhouseblog.pojo.vo.UserRegisterVo;
+import com.wolfhouse.wolfhouseblog.services.UserAuthService;
+import com.wolfhouse.wolfhouseblog.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author linexsong
+ */
+@RestController
+@RequestMapping("/user")
+@Tag(name = "用户接口")
+@Slf4j
+@RequiredArgsConstructor
+public class UserController {
+    private final UserAuthService authService;
+    private final AuthenticationManager authManager;
+    private final UserService userService;
+
+    @Operation(summary = "登陆")
+    @PostMapping("/login")
+    public ResponseEntity<HttpResult<UserLoginVo>> login(@RequestBody UserLoginDto dto) {
+        try {
+            Authentication auth = authManager.authenticate(UsernamePasswordAuthenticationToken.unauthenticated(
+                    dto.getAccount(),
+                    dto.getPassword()));
+            log.info("用户[{}]登陆，状态: {}", auth.getPrincipal(), auth.isAuthenticated());
+            return ResponseEntity.ok()
+                                 .body(HttpResult.success(UserLoginVo.token("123")));
+
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED.value())
+                                 .body(HttpResult.failed(
+                                         HttpCodeConstant.AUTH_FAILED,
+                                         AuthExceptionConstant.AUTHENTIC_FAILED,
+                                         null));
+        }
+
+    }
+
+    @Operation(summary = "注册")
+    @PostMapping("/register")
+    @Transactional(rollbackFor = Exception.class)
+    public HttpResult<UserRegisterVo> register(@RequestBody @Valid UserRegisterDto dto) {
+        log.info("用户注册: {}", dto);
+        // 检查用户是否存在
+        if (userService.hasAccountOrEmail(dto.getEmail())) {
+            return HttpResult.failed(HttpCodeConstant.USER_ALREADY_EXIST, UserConstant.USER_ALREADY_EXIST, null);
+        }
+        // 设置用户 ID
+        dto.setUserId(authService.createAuth(dto.getPassword())
+                                 .getUserId());
+        // 注册用户
+        return HttpResult.success(userService.createUser(dto));
+    }
+
+
+}
