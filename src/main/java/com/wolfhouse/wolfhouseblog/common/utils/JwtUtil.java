e37c615a47411b8201390bb5604a1b0548428f6a
LinexSong <linexwolf@icloud.com>
2025-07-28 18:02:09 +0800

    feat: 新增 JWT 配置类及工具类

---
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/utils/JwtUtil.java
@@ -0,0 +1,13 @@
+package com.wolfhouse.wolfhouseblog.common.utils;
+
+import com.wolfhouse.wolfhouseblog.common.properties.JwtProperties;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author linexsong
+ */
+@RequiredArgsConstructor
+public class JwtUtil {
+    private final JwtProperties properties;
+
+}
