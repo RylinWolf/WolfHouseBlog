ccc6860724ac153ae37ac6271cbe7b9d236ee670
LinexSong <linexwolf@icloud.com>
2025-07-31 10:09:03 +0800

    feat: 新增常量字段

--- src/main/java/com/wolfhouse/wolfhouseblog/handler/GlobalExceptionHandler.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/handler/GlobalExceptionHandler.java
@@ -1,6 +1,11 @@
 package com.wolfhouse.wolfhouseblog.handler;
 
+import com.wolfhouse.wolfhouseblog.common.constant.ServiceExceptionConstant;
+import com.wolfhouse.wolfhouseblog.common.http.HttpCodeConstant;
+import com.wolfhouse.wolfhouseblog.common.http.HttpResult;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -11,7 +16,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler
-    public void handleException(Exception e) {
+    public ResponseEntity<HttpResult<?>> handleException(Exception e) {
         log.error("发生异常: [{}]", e.getMessage(), e);
+        return HttpResult.failed(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpCodeConstant.SERVER_ERROR,
+                ServiceExceptionConstant.SERVER_ERROR);
     }
 }
