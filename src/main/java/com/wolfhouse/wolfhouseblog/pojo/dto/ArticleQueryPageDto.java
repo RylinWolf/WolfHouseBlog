1035f1fa3f870ce3f304652e984064b434133f78
LinexSong <linexwolf@icloud.com>
2025-07-31 17:36:08 +0800

    feat: 文章查询 Dto 现在支持按日期查询

--- src/main/java/com/wolfhouse/wolfhouseblog/pojo/dto/ArticleQueryPageDto.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/pojo/dto/ArticleQueryPageDto.java
@@ -4,17 +4,20 @@ import com.wolfhouse.wolfhouseblog.common.utils.page.PageDto;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.openapitools.jackson.nullable.JsonNullable;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
 /**
  * @author linexsong
  */
+@Component
 @Data
-@EqualsAndHashCode(callSuper = true)
+@EqualsAndHashCode(callSuper = false)
 public class ArticleQueryPageDto extends PageDto {
     private JsonNullable<Long> id = JsonNullable.undefined();
     private JsonNullable<String> title = JsonNullable.undefined();
     private JsonNullable<Long> authorId = JsonNullable.undefined();
-    private JsonNullable<LocalDateTime> postTime = JsonNullable.undefined();
+    private JsonNullable<LocalDateTime> postStart = JsonNullable.undefined();
+    private JsonNullable<LocalDateTime> postEnd = JsonNullable.undefined();
 }
