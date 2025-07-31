e21f05481cfc3992abc066800499ca1499bf3c46
LinexSong <linexwolf@icloud.com>
2025-07-31 10:10:44 +0800

    feat: 新增常量字段

--- src/main/java/com/wolfhouse/wolfhouseblog/common/constant/ServiceExceptionConstant.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/constant/ServiceExceptionConstant.java
@@ -7,4 +7,5 @@ public class ServiceExceptionConstant {
     public static final String EXEC_FAILED = "执行失败";
     public static final String ARG_FORMAT_ERROR = "字段格式有误";
     public static final String NO_RESOURCE = "资源不存在";
+    public static final String SERVER_ERROR = "服务器异常";
 }
