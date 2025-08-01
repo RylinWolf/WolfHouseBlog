e4dc6b43e26580c52f9f0d0e4f861373cd30d637
LinexSong <linexwolf@icloud.com>
2025-08-01 16:30:33 +0800

    feat: 增强验证链工具

--- src/main/java/com/wolfhouse/wolfhouseblog/auth/service/verify/VerifyConstant.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/auth/service/verify/VerifyConstant.java
@@ -4,5 +4,5 @@ package com.wolfhouse.wolfhouseblog.auth.service.verify;
  * @author linexsong
  */
 public class VerifyConstant {
-    public static final String VERIFY_FAILED = "验证未通过";
+    public static final String VERIFY_FAILED = "字段验证未通过";
 }
--- src/main/java/com/wolfhouse/wolfhouseblog/auth/service/verify/VerifyTool.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/auth/service/verify/VerifyTool.java
@@ -1,17 +1,36 @@
 package com.wolfhouse.wolfhouseblog.auth.service.verify;
 
 import com.wolfhouse.wolfhouseblog.auth.service.verify.impl.BaseVerifyChain;
+import com.wolfhouse.wolfhouseblog.auth.service.verify.impl.nodes.comons.LoginVerifyNode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author linexsong
  */
 public class VerifyTool {
+    /**
+     * 根据 VerifyNode 数组构建验证链
+     *
+     * @param nodes 验证节点数组
+     * @return 验证链
+     */
     public static BaseVerifyChain of(VerifyNode<?>... nodes) {
         BaseVerifyChain chain = BaseVerifyChain.instance();
         chain.add(nodes);
         return chain;
     }
 
+    /**
+     * 根据 VerifyNode 数组构建验证链，并设置统一错误信息。
+     * 该错误信息的优先级低于 VerifyNode 配置的信息。
+     *
+     * @param nodes 验证节点数组
+     * @param msg   错误信息
+     * @return 验证链
+     */
     public static BaseVerifyChain ofAllMsg(String msg, VerifyNode<?>... nodes) {
         for (VerifyNode<?> node : nodes) {
             if (node.getException() == null) {
@@ -19,5 +38,19 @@ public class VerifyTool {
             }
         }
         return of(nodes);
+    }
+
+    /**
+     * 根据 VerifyNode 数组构建验证链。
+     * 自动添加登陆验证节点。
+     *
+     * @param nodes 验证节点数组
+     * @return 验证链
+     */
+    public static BaseVerifyChain ofLogin(VerifyNode<?>... nodes) {
+        List<VerifyNode<?>> nodeList = new ArrayList<>(Arrays.stream(nodes)
+                                                             .toList());
+        nodeList.addFirst(new LoginVerifyNode());
+        return of(nodeList.toArray(new VerifyNode[0]));
     }
 }
