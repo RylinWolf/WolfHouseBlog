6adae5d7e5a5b670b3ffd14320a4cf254685a44d
LinexSong <linexwolf@icloud.com>
2025-08-05 16:21:22 +0800

    feat: 为 HttpResult 新增失败方法重载

--- src/main/java/com/wolfhouse/wolfhouseblog/common/http/HttpResult.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/http/HttpResult.java
@@ -88,4 +88,8 @@ public class HttpResult<T> implements Serializable {
     public static <T> ResponseEntity<HttpResult<T>> ok(T data, String msg) {
         return ResponseEntity.ok(HttpResult.success(data, msg));
     }
+
+    public static <T> HttpResult<T> failedIfBlank(T data) {
+        return failedIfBlank(HttpCodeConstant.FAILED, null, data);
+    }
 }
