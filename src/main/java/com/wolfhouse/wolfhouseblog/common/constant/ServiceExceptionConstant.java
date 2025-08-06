b2bd407c97c47e767dd29a1b6f03649bf605c80f
LinexSong <linexwolf@icloud.com>
2025-08-06 17:22:14 +0800

    feat: 新增非相等验证节点
    feat: 添加常量字段

--- src/main/java/com/wolfhouse/wolfhouseblog/common/constant/ServiceExceptionConstant.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/constant/ServiceExceptionConstant.java
@@ -8,4 +8,5 @@ public class ServiceExceptionConstant {
     public static final String ARG_FORMAT_ERROR = "字段格式有误";
     public static final String NO_RESOURCE = "资源不存在";
     public static final String SERVER_ERROR = "服务器异常";
+    public static final String SERVICE_ERROR = "业务异常";
 }
