9730c1374c8749d46d5df86a15a3578d9b80b1aa
LinexSong <linexwolf@icloud.com>
2025-08-06 14:24:41 +0800

    feat: 新增用户 ID 验证节点

--- src/main/java/com/wolfhouse/wolfhouseblog/auth/service/verify/impl/nodes/user/UserVerifyNode.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/auth/service/verify/impl/nodes/user/UserVerifyNode.java
@@ -1,5 +1,6 @@
 package com.wolfhouse.wolfhouseblog.auth.service.verify.impl.nodes.user;
 
+import com.wolfhouse.wolfhouseblog.service.UserAuthService;
 import com.wolfhouse.wolfhouseblog.service.UserService;
 
 /**
@@ -8,12 +9,20 @@ import com.wolfhouse.wolfhouseblog.service.UserService;
 public class UserVerifyNode {
     public static final UsernameVerifyNode USERNAME = new UsernameVerifyNode();
     public static final BirthVerifyNode BIRTH = new BirthVerifyNode();
+    private static UserIdVerifyNode USER_ID;
     private static EmailVerifyNode EMAIL;
-
+    
     public static EmailVerifyNode email(UserService service) {
         if (EMAIL == null) {
             EMAIL = new EmailVerifyNode(service);
         }
         return EMAIL;
+    }
+
+    public static UserIdVerifyNode id(UserAuthService service) {
+        if (USER_ID == null) {
+            USER_ID = new UserIdVerifyNode(service);
+        }
+        return USER_ID;
     }
 }
