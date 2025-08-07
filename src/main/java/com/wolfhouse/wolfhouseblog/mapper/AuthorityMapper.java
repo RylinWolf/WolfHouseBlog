452c51cf63369467e9ee232ee6764769cec0066d
LinexSong <linexwolf@icloud.com>
2025-08-07 18:00:54 +0800

    feat: 新增为管理员添加权限 Mapper 方法

--- src/main/java/com/wolfhouse/wolfhouseblog/mapper/AuthorityMapper.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/mapper/AuthorityMapper.java
@@ -9,4 +9,12 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface AuthorityMapper extends BaseMapper<Authority> {
+    /**
+     * 为指定管理员添加权限。
+     *
+     * @param adminId     管理员的 ID
+     * @param authorities 权限 ID 的列表
+     * @return 受影响的行数
+     */
+    Integer addAuthorities(Long adminId, Long... authorities);
 }
