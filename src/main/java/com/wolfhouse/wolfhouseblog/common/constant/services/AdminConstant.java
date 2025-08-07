4f6bbdc9523a81d72ad60c7539c516668a2ee6f1
LinexSong <linexwolf@icloud.com>
2025-08-07 13:24:38 +0800

    feat: 新增管理员相关常量

---
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/constant/services/AdminConstant.java
@@ -0,0 +1,9 @@
+package com.wolfhouse.wolfhouseblog.common.constant.services;
+
+/**
+ * @author linexsong
+ */
+public class AdminConstant {
+    public static final String CREATE_FAILED = "创建失败！";
+    public static final String ADMIN_EXISTS = "管理员已存在！";
+}
