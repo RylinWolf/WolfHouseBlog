04ea9ed43109d9246e81b12446ebaaaddf68ab44
LinexSong <linexwolf@icloud.com>
2025-08-07 16:16:57 +0800

    feat: 添加常量字段

--- src/main/java/com/wolfhouse/wolfhouseblog/common/constant/services/AdminConstant.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/constant/services/AdminConstant.java
@@ -6,4 +6,6 @@ package com.wolfhouse.wolfhouseblog.common.constant.services;
 public class AdminConstant {
     public static final String CREATE_FAILED = "创建失败！";
     public static final String ADMIN_EXISTS = "管理员已存在！";
+    public static final String ADMIN_NOT_EXIST = "管理员不存在！";
+    public static final String UPDATE_FAILED = "更新失败！";
 }
