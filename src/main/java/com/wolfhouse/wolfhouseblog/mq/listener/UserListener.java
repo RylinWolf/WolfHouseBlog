43ed035ddad0c9fb85d418b375c3fb93569b30df
LinexSong <linexwolf@icloud.com>
2025-08-08 16:38:26 +0800

    feat: 新增 MQ 用户删除监听器

--- src/main/java/com/wolfhouse/wolfhouseblog/mq/listener/UserListener.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/mq/listener/UserListener.java
@@ -1,7 +1,12 @@
 package com.wolfhouse.wolfhouseblog.mq.listener;
 
 import com.wolfhouse.wolfhouseblog.common.constant.mq.MqUserConstant;
+import com.wolfhouse.wolfhouseblog.common.exceptions.ServiceException;
+import com.wolfhouse.wolfhouseblog.mq.MqTools;
 import com.wolfhouse.wolfhouseblog.pojo.dto.UserRegisterDto;
+import com.wolfhouse.wolfhouseblog.pojo.dto.mq.MqUserAuthDto;
+import com.wolfhouse.wolfhouseblog.service.AdminService;
+import com.wolfhouse.wolfhouseblog.service.UserAuthService;
 import com.wolfhouse.wolfhouseblog.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,16 +25,38 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class UserListener {
     private final UserService userService;
+    private final UserAuthService userAuthService;
+    private final AdminService adminService;
+    private final MqTools mqTools;
 
     @RabbitListener(
-            bindings = @QueueBinding(
-                    value = @Queue(name = MqUserConstant.CREATE_QUEUE),
-                    exchange = @Exchange(
-                            name = MqUserConstant.CREATE_EXCHANGE,
-                            type = ExchangeTypes.TOPIC),
-                    key = {MqUserConstant.KEY_CREATE_USER}))
+         bindings = @QueueBinding(
+              value = @Queue(name = MqUserConstant.CREATE_QUEUE),
+              exchange = @Exchange(
+                   name = MqUserConstant.CREATE_EXCHANGE,
+                   type = ExchangeTypes.TOPIC),
+              key = {MqUserConstant.KEY_CREATE_USER}))
     public void userCreateListener(UserRegisterDto dto) {
         log.info("监听到用户创建：{}", dto);
     }
 
+
+    @RabbitListener(
+         bindings = @QueueBinding(
+              value = @Queue(name = MqUserConstant.DELETE_QUEUE),
+              exchange = @Exchange(
+                   name = MqUserConstant.DELETE_EXCHANGE,
+                   type = ExchangeTypes.TOPIC),
+              key = {MqUserConstant.KEY_DELETE_USER}
+         ))
+    public void userDeleteListener(MqUserAuthDto dto) throws Exception {
+        log.info("监听到用户删除: {}", dto.getUserId());
+        mqTools.setLoginAuth(dto);
+
+        try {
+            userService.deleteAccount(dto.getUserId());
+        } catch (Exception e) {
+            throw new ServiceException(e.getMessage(), e);
+        }
+    }
 }
