51cf4be74a8d508cbf3dfdbb4b6cc3be0bf2384d
LinexSong <linexwolf@icloud.com>
2025-08-08 16:34:40 +0800

    feat: 服务工具类新增获取、设置验证信息相关方法

--- src/main/java/com/wolfhouse/wolfhouseblog/common/utils/ServiceUtil.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/utils/ServiceUtil.java
@@ -2,8 +2,13 @@ package com.wolfhouse.wolfhouseblog.common.utils;
 
 import com.wolfhouse.wolfhouseblog.common.exceptions.ServiceException;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Collection;
 
 /**
  * @author linexsong
@@ -28,5 +33,26 @@ public class ServiceUtil {
             throw ServiceException.loginRequired();
         }
         return l;
+    }
+
+    public static Authentication getAuth() {
+        return SecurityContextHolder.getContext()
+                                    .getAuthentication();
+    }
+
+    public static void setLoginUser(Long loginId) {
+        SecurityContext context = SecurityContextHolder.getContext();
+        
+        context.setAuthentication(new UsernamePasswordAuthenticationToken(loginId, null));
+    }
+
+    public static void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication auth = context.getAuthentication();
+
+        if (auth == null) {
+            auth = new UsernamePasswordAuthenticationToken(null, null, authorities);
+        }
+        context.setAuthentication(auth);
     }
 }
