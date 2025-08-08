8f704dedc1bc7742f6240892313ba5f45e58581b
LinexSong <linexwolf@icloud.com>
2025-08-08 16:34:53 +0800

    feat: 添加常量字段

--- src/main/java/com/wolfhouse/wolfhouseblog/common/constant/mq/MqUserConstant.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/constant/mq/MqUserConstant.java
@@ -8,12 +8,19 @@ import com.wolfhouse.wolfhouseblog.common.constant.services.UserConstant;
 public class MqUserConstant {
     public static final String USER = UserConstant.USER;
     public static final String BASE =
-            MqConstant.SERVICE + MqConstant.SEPARATOR + MqUserConstant.USER + MqConstant.SEPARATOR;
+         MqConstant.SERVICE + MqConstant.SEPARATOR + MqUserConstant.USER + MqConstant.SEPARATOR;
 
 
     public static final String CREATE = "create";
+    public static final String DELETE = "delete";
+
     public static final String CREATE_QUEUE = BASE + CREATE + MqConstant.SEPARATOR + MqConstant.QUEUE;
     public static final String CREATE_EXCHANGE = BASE + CREATE + MqConstant.SEPARATOR + MqConstant.EXCHANGE;
     public static final String KEY_CREATE_USER =
-            BASE + CREATE + UserConstant.USER + MqConstant.SEPARATOR + MqConstant.KEY;
+         BASE + CREATE + UserConstant.USER + MqConstant.SEPARATOR + MqConstant.KEY;
+
+    public static final String DELETE_EXCHANGE = BASE + DELETE + MqConstant.SEPARATOR + MqConstant.EXCHANGE;
+    public static final String KEY_DELETE_USER =
+         BASE + DELETE + UserConstant.USER + MqConstant.SEPARATOR + MqConstant.KEY;
+    public static final String DELETE_QUEUE = BASE + DELETE + MqConstant.SEPARATOR + MqConstant.QUEUE;
 }
