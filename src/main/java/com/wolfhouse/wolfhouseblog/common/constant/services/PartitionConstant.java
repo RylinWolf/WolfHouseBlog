62c97071c3cee0a9898955bda052f0a199d81c25
LinexSong <linexwolf@icloud.com>
2025-08-10 12:19:25 +0800

    feat: 新增分区相关常量类

---
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/constant/services/PartitionConstant.java
@@ -0,0 +1,8 @@
+package com.wolfhouse.wolfhouseblog.common.constant.services;
+
+/**
+ * @author linexsong
+ */
+public class PartitionConstant {
+    public static final String ADD_FAILED = "分区添加失败！";
+}
