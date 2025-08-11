18c8e010132eba4dcdf90c7c41a695bec8688226
LinexSong <linexwolf@icloud.com>
2025-08-11 18:16:07 +0800

    feat: 为分区 VO 重写判等方法

--- src/main/java/com/wolfhouse/wolfhouseblog/pojo/vo/PartitionVo.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/pojo/vo/PartitionVo.java
@@ -1,15 +1,33 @@
 package com.wolfhouse.wolfhouseblog.pojo.vo;
 
 import com.wolfhouse.wolfhouseblog.common.enums.VisibilityEnum;
+import lombok.Builder;
 import lombok.Data;
+
+import java.util.Objects;
 
 /**
  * @author linexsong
  */
 @Data
+@Builder
 public class PartitionVo {
     private Long id;
     private String name;
     private PartitionVo[] children;
     private VisibilityEnum visibility;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PartitionVo that = (PartitionVo) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
