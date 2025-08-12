89dd75b346d3a5d08dea1c09daf066f8cc63d0c1
LinexSong <linexwolf@icloud.com>
2025-08-12 18:28:11 +0800

    fix: 修复了获取指定分区时，报错 500 的问题

--- src/main/java/com/wolfhouse/wolfhouseblog/mapper/PartitionMapper.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/mapper/PartitionMapper.java
@@ -14,11 +14,12 @@ import java.util.Set;
 public interface PartitionMapper extends BaseMapper<Partition> {
 
     /**
-     * 获取指定分区 ID 的孩子 ID
+     * 获取指定分区 ID 的孩子 ID。该方法将查询数据库中指定父分区下的所有直接子分区ID，
+     * 并按照order字段进行排序。
      *
-     * @param partitionId
-     * @return
+     * @param partitionId 父分区的ID，用于查找其下的所有直接子分区
+     * @return 返回包含所有子分区ID的Set集合，按order字段排序。如果没有子分区则返回空Set
      */
-    @Select("select id from `partition` where parent_id = #{partitionId} order by sort")
+    @Select("select id from `partition` where parent_id = #{partitionId} order by `order`, id")
     Set<Long> getChildrenIds(Long partitionId);
 }
