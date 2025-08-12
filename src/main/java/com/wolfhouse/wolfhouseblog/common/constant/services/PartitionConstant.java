08ba63b9f8fa0203d9b04bff674c2d538d54d418
LinexSong <linexwolf@icloud.com>
2025-08-12 16:44:48 +0800

    feat: 添加常量字段

--- src/main/java/com/wolfhouse/wolfhouseblog/common/constant/services/PartitionConstant.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/constant/services/PartitionConstant.java
@@ -7,4 +7,5 @@ public class PartitionConstant {
     public static final String ADD_FAILED = "分区添加失败！";
     public static final String ALREADY_EXIST = "分区已存在！";
     public static final String NOT_EXIST = "分区不存在！";
+    public static final String UPDATE_FAILED = "分区修改失败！";
 }
