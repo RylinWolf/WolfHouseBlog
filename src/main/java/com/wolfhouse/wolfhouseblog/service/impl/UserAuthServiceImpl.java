3dd95a95e27b19e5b574b6df78419247613110be
LinexSong <linexwolf@icloud.com>
2025-08-13 09:34:11 +0800

    feat: 用户验证类的获取登陆用户方法中，若当前登录信息过期，则会自动清除安全上下文中的认证信息

--- src/main/java/com/wolfhouse/wolfhouseblog/service/impl/UserAuthServiceImpl.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/service/impl/UserAuthServiceImpl.java
@@ -45,6 +45,7 @@ public class UserAuthServiceImpl extends ServiceImpl<UserAuthMapper, UserAuth> i
         if (node.verify()) {
             return login;
         }
+        ServiceUtil.removeLogin();
         throw node.getException();
     }
 
