5215c1711385e30d232ac10dceab44d47bf32900
LinexSong <linexwolf@icloud.com>
2025-08-14 17:24:40 +0800

    feat: 新增标签视图对象

---
+++ src/main/java/com/wolfhouse/wolfhouseblog/pojo/vo/TagVo.java
@@ -0,0 +1,12 @@
+package com.wolfhouse.wolfhouseblog.pojo.vo;
+
+import lombok.Data;
+
+/**
+ * @author linexsong
+ */
+@Data
+public class TagVo {
+    private Long id;
+    private String name;
+}
