0c327bd64992cdc5b8e793e4fa89aff292e3ba89
LinexSong <linexwolf@icloud.com>
2025-08-14 17:56:46 +0800

    feat: 为用户标签类新增有/无参构造方法

--- src/main/java/com/wolfhouse/wolfhouseblog/pojo/domain/UserTag.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/pojo/domain/UserTag.java
@@ -1,13 +1,17 @@
 package com.wolfhouse.wolfhouseblog.pojo.domain;
 
 import com.mybatisflex.annotation.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author linexsong
  */
 @Table("user_tag")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserTag {
     private Long userId;
     private Long tagId;
