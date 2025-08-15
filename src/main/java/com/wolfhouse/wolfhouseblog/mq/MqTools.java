1642e000e2707c05e3fbd64c3b671510419fd9b1
LinexSong <linexwolf@icloud.com>
2025-08-15 10:35:08 +0800

    feat: MQ 工具设置登陆信息方法新增用户可达性验证

--- src/main/java/com/wolfhouse/wolfhouseblog/mq/MqTools.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/mq/MqTools.java
@@ -1,9 +1,12 @@
 package com.wolfhouse.wolfhouseblog.mq;
 
+import com.wolfhouse.wolfhouseblog.common.constant.services.UserConstant;
+import com.wolfhouse.wolfhouseblog.common.exceptions.ServiceException;
 import com.wolfhouse.wolfhouseblog.common.utils.ServiceUtil;
 import com.wolfhouse.wolfhouseblog.pojo.domain.Authority;
 import com.wolfhouse.wolfhouseblog.pojo.dto.mq.MqUserAuthDto;
 import com.wolfhouse.wolfhouseblog.service.AdminService;
+import com.wolfhouse.wolfhouseblog.service.UserAuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -16,9 +19,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MqTools {
     private final AdminService adminService;
+    private final UserAuthService authService;
 
     public void setLoginAuth(MqUserAuthDto dto) throws Exception {
         Long loginId = dto.getUserId();
+        if (authService.isUserUnaccessible(loginId)) {
+            throw new ServiceException(UserConstant.USER_UNACCESSIBLE);
+        }
+        
         ServiceUtil.setLoginUser(loginId);
         List<Authority> authorities = adminService.getAuthorities(loginId);
         ServiceUtil.setAuthorities(authorities);
