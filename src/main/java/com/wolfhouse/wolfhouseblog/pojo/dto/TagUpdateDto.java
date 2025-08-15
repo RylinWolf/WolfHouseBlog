1538888053dab80958569291fdb0a87a2bbeb483
LinexSong <linexwolf@icloud.com>
2025-08-15 16:11:35 +0800

    feat: 新增标签更新 Dto

---
+++ src/main/java/com/wolfhouse/wolfhouseblog/pojo/dto/TagUpdateDto.java
@@ -0,0 +1,12 @@
+package com.wolfhouse.wolfhouseblog.pojo.dto;
+
+import lombok.Data;
+
+/**
+ * @author linexsong
+ */
+@Data
+public class TagUpdateDto {
+    private Long id;
+    private String name;
+}
