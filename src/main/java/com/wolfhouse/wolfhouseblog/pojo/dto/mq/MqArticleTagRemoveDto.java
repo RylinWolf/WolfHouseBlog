94171f248f8df133e76b6520d3c8285b52cac54e
LinexSong <linexwolf@icloud.com>
2025-08-15 10:35:40 +0800

    feat: 优化 MQ 文章标签移除 Dto 结构

--- src/main/java/com/wolfhouse/wolfhouseblog/pojo/dto/mq/MqArticleTagRemoveDto.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/pojo/dto/mq/MqArticleTagRemoveDto.java
@@ -1,7 +1,7 @@
 package com.wolfhouse.wolfhouseblog.pojo.dto.mq;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.util.Set;
@@ -10,9 +10,13 @@ import java.util.Set;
  * @author linexsong
  */
 @Data
+@EqualsAndHashCode(callSuper = false)
 @NoArgsConstructor
-@AllArgsConstructor
-public class MqArticleTagRemoveDto {
-    private Long userId;
+public class MqArticleTagRemoveDto extends MqUserAuthDto {
     private Set<Long> tagIds;
+
+    public MqArticleTagRemoveDto(Long userId, Set<Long> tagIds) {
+        super(userId);
+        this.tagIds = tagIds;
+    }
 }
