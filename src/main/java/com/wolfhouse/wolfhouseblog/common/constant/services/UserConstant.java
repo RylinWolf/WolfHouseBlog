5b49c3a67692f13becf5e55a633418d3acb89adf
LinexSong <linexwolf@icloud.com>
2025-08-15 15:52:17 +0800

    feat: 添加常量字段

--- src/main/java/com/wolfhouse/wolfhouseblog/common/constant/services/UserConstant.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/constant/services/UserConstant.java
@@ -26,4 +26,7 @@ public class UserConstant {
     public static final String DISABLE_FAILED = "账号禁用失败！";
     public static final String USER_HAS_ENABLED = "账户已启用！";
     public static final String ENABLE_FAILED = "账号启用失败！";
+    public static final String UNSUBSCRIBE_FAILED = "取消关注失败！";
+    public static final String SUBSCRIBE_CANNOT_BE_SELF = "不能关注自己！";
+    public static final String NOT_SUBSCRIBED = "未关注该用户！";
 }
