379c239271b35dd34f77917927b085361a38dc03
LinexSong <linexwolf@icloud.com>
2025-08-15 10:26:24 +0800

    feat: 为文章 MQ 服务新增移除常用标签业务方法

--- src/main/java/com/wolfhouse/wolfhouseblog/mq/service/MqArticleService.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/mq/service/MqArticleService.java
@@ -1,6 +1,7 @@
 package com.wolfhouse.wolfhouseblog.mq.service;
 
 import com.wolfhouse.wolfhouseblog.common.constant.mq.MqArticleConstant;
+import com.wolfhouse.wolfhouseblog.pojo.dto.mq.MqArticleTagRemoveDto;
 import com.wolfhouse.wolfhouseblog.pojo.dto.mq.MqPartitionChangeDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -18,6 +19,13 @@ public class MqArticleService {
         template.convertAndSend(
              MqArticleConstant.PARTITION_CHANGE_EXCHANGE,
              MqArticleConstant.KEY_PARTITION_CHANGE,
+             dto);
+    }
+
+    public void articleComUseTagsRemove(MqArticleTagRemoveDto dto) {
+        template.convertAndSend(
+             MqArticleConstant.TAG_REMOVE_EXCHANGE,
+             MqArticleConstant.KEY_TAG_REMOVE,
              dto);
     }
 }
