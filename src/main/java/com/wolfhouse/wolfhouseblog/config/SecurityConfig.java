83e6c5cb8a4fe90b52775ddb81a6a4a5d27fcce1
LinexSong <linexwolf@icloud.com>
2025-08-18 20:38:07 +0800

    feat: 启用方法安全验证

--- src/main/java/com/wolfhouse/wolfhouseblog/config/SecurityConfig.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import com.wolfhouse.wolfhouseblog.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,6 +22,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  */
 @Configuration
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     public JwtFilter jwtFilter(JwtUtil jwtUtil, UserService userService, AdminService adminService) {
