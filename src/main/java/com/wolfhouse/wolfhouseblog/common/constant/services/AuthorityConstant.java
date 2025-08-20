03411c9c5ea7637b272b8b84d9789414ac93d7ca
LinexSong <linexwolf@icloud.com>
2025-08-20 19:23:02 +0800

    feat: 新增权限相关常量类

---
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/constant/services/AuthorityConstant.java
@@ -0,0 +1,9 @@
+package com.wolfhouse.wolfhouseblog.common.constant.services;
+
+/**
+ * @author linexsong
+ */
+public class AuthorityConstant {
+    public static final String ADD_FAILED = "添加失败！";
+    public static final String NOT_EXIST = "权限不存在！";
+}
