15149808fe4bb090788508ce846f603bcfdfa39b
LinexSong <linexwolf@icloud.com>
2025-08-20 19:22:43 +0800

    feat: 添加常量字段

--- src/main/java/com/wolfhouse/wolfhouseblog/common/constant/services/BlogPermissionConstant.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/constant/services/BlogPermissionConstant.java
@@ -8,7 +8,7 @@ public class BlogPermissionConstant {
     public static final String SUPER_ADMIN = "SUPER_ADMIN";
     public static final String ROLE_ADMIN = "ROLE_" + ADMIN;
     public static final String ROLE_SUPER_ADMIN = "ROLE" + SUPER_ADMIN;
-    
+
     public static final String ADMIN_NAME = "博客管理员";
     public static final String SUPER_ADMIN_NAME = "超级管理员";
 
@@ -22,4 +22,7 @@ public class BlogPermissionConstant {
     public static final String USER_UPDATE_NAME = "修改用户";
     public static final String USER_DISABLE_NAME = "启/停用用户";
 
+    public static final String AUTHORITY_CREATE = "admin:authority:create";
+
+    public static final String AUTHORITY_CREATE_NAME = "添加管理员权限";
 }
