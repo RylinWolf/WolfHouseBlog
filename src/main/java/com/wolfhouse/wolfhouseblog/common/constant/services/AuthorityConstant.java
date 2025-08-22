1a90e6952c45fc1ca9424df05a29a3720d9363fe
LinexSong <linexwolf@icloud.com>
2025-08-22 18:26:03 +0800

    feat: 添加常量字段

--- src/main/java/com/wolfhouse/wolfhouseblog/common/constant/services/AuthorityConstant.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/constant/services/AuthorityConstant.java
@@ -8,4 +8,5 @@ public class AuthorityConstant {
     public static final String NOT_EXIST = "权限不存在！";
     public static final String DELETE_FAILED = "删除失败！";
     public static final String HAS_EXIST = "已有权限！";
+    public static final String UPDATE_FAILED = "更新失败！";
 }
