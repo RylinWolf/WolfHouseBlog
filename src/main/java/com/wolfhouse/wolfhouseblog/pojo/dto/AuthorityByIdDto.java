696799d12033b65cc161a7a06684a8e029c16f8d
LinexSong <linexwolf@icloud.com>
2025-08-22 18:24:00 +0800

    feat: 为权限 ID Dto 添加构造方法

--- src/main/java/com/wolfhouse/wolfhouseblog/pojo/dto/AuthorityByIdDto.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/pojo/dto/AuthorityByIdDto.java
@@ -1,6 +1,8 @@
 package com.wolfhouse.wolfhouseblog.pojo.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
@@ -8,6 +10,8 @@ import java.util.Set;
  * @author linexsong
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthorityByIdDto {
     private Long adminId;
     private Set<Long> ids;
