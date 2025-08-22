4573c331c1ab5856e3b815d624c887a535590e4a
LinexSong <linexwolf@icloud.com>
2025-08-22 18:18:25 +0800

    feat: 为管理员权限 Mapper 新增移除全部权限、获取全部权限方法

--- src/main/java/com/wolfhouse/wolfhouseblog/mapper/AdminAuthMapper.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/mapper/AdminAuthMapper.java
@@ -2,14 +2,44 @@ package com.wolfhouse.wolfhouseblog.mapper;
 
 import com.mybatisflex.core.BaseMapper;
 import com.wolfhouse.wolfhouseblog.pojo.domain.AdminAuth;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
 import java.util.Set;
 
 /**
+ * 管理员权限映射接口
+ *
  * @author linexsong
  */
 @Mapper
 public interface AdminAuthMapper extends BaseMapper<AdminAuth> {
+    /**
+     * 为管理员添加多个权限
+     *
+     * @param adminId 管理员ID
+     * @param ids     权限ID集合
+     * @return 添加成功的权限数量
+     */
     Integer addByIds(Long adminId, Set<Long> ids);
+
+    /**
+     * 删除管理员的所有权限
+     *
+     * @param adminId 管理员ID
+     * @return 删除的权限数量
+     */
+    @Delete("delete from admin_authority where admin_id = #{adminId}")
+    Integer deleteAllByAdmin(Long adminId);
+
+    /**
+     * 获取管理员的所有权限ID
+     *
+     * @param adminId 管理员ID
+     * @return 权限ID列表
+     */
+    @Select("select authority_id from admin_authority where admin_id = #{adminId}")
+    List<Long> getIdsByAdminId(Long adminId);
 }
