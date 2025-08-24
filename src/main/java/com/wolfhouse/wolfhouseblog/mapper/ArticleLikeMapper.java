71d97b79a2a1f770a1db60f2d3afaa1a16eb054f
DESKTOP-PNPHEKJ\LiJH <RuixeWolf@outlook.com>
2025-08-25 00:03:08 +0800

    feat: 新增文章评论、收藏、点赞 Mapper

---
+++ src/main/java/com/wolfhouse/wolfhouseblog/mapper/ArticleLikeMapper.java
@@ -0,0 +1,9 @@
+package com.wolfhouse.wolfhouseblog.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.wolfhouse.wolfhouseblog.pojo.domain.ArticleLike;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ArticleLikeMapper extends BaseMapper<ArticleLike> {
+}
