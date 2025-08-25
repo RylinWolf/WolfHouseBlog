33e432b107598e299ec7f78d37d9dc44f8881bd5
RylinWolf <linexwolf@icloud.com>
2025-08-25 15:14:10 +0800

    feat: 文章交互接口中的部分方法使用分页结果

--- src/main/java/com/wolfhouse/wolfhouseblog/service/ArticleActionService.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/service/ArticleActionService.java
@@ -1,16 +1,36 @@
 package com.wolfhouse.wolfhouseblog.service;
 
+import com.wolfhouse.wolfhouseblog.common.utils.page.PageResult;
 import com.wolfhouse.wolfhouseblog.pojo.dto.ArticleCommentDto;
+import com.wolfhouse.wolfhouseblog.pojo.dto.ArticleCommentPageDto;
 import com.wolfhouse.wolfhouseblog.pojo.vo.ArticleCommentVo;
 
 import java.util.List;
 
 public interface ArticleActionService {
-    List<ArticleCommentVo> getArticleCommentVos(Long articleId);
+    /**
+     * 获取指定文章的评论列表
+     *
+     * @param dto 文章评论分页查询参数
+     * @return 分页评论列表数据
+     */
+    PageResult<ArticleCommentVo> getArticleCommentVos(ArticleCommentPageDto dto);
 
-    List<ArticleCommentVo> postComment(ArticleCommentDto dto);
+    /**
+     * 发表新评论
+     *
+     * @param dto 评论内容数据传输对象
+     * @return 更新后的评论分页列表
+     */
+    PageResult<ArticleCommentVo> postComment(ArticleCommentDto dto);
 
-    List<ArticleCommentVo> deleteComment(Long commentId);
+    /**
+     * 删除指定评论
+     *
+     * @param commentId 需要删除的评论ID
+     * @return 更新后的评论分页列表
+     */
+    PageResult<ArticleCommentVo> deleteComment(Long commentId);
 
     Boolean isLiked(Long articleId);
 
@@ -23,5 +43,4 @@ public interface ArticleActionService {
     Boolean favorite(Long articleId);
 
     Boolean removeFavorite(Long articleId);
-
 }
--- src/main/java/com/wolfhouse/wolfhouseblog/service/impl/ArticleActionServiceImpl.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/service/impl/ArticleActionServiceImpl.java
@@ -1,28 +1,47 @@
 package com.wolfhouse.wolfhouseblog.service.impl;
 
+import com.mybatisflex.core.query.QueryWrapper;
+import com.wolfhouse.wolfhouseblog.auth.service.ServiceAuthMediator;
+import com.wolfhouse.wolfhouseblog.common.utils.page.PageResult;
+import com.wolfhouse.wolfhouseblog.common.utils.verify.VerifyTool;
+import com.wolfhouse.wolfhouseblog.common.utils.verify.impl.nodes.article.ArticleVerifyNode;
+import com.wolfhouse.wolfhouseblog.mapper.ArticleCommentMapper;
+import com.wolfhouse.wolfhouseblog.mapper.ArticleFavoriteMapper;
+import com.wolfhouse.wolfhouseblog.mapper.ArticleLikeMapper;
 import com.wolfhouse.wolfhouseblog.pojo.dto.ArticleCommentDto;
+import com.wolfhouse.wolfhouseblog.pojo.dto.ArticleCommentPageDto;
 import com.wolfhouse.wolfhouseblog.pojo.vo.ArticleCommentVo;
 import com.wolfhouse.wolfhouseblog.service.ArticleActionService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import static com.wolfhouse.wolfhouseblog.pojo.domain.table.ArticleCommentTableDef.ARTICLE_COMMENT;
 
+/**
+ * @author rylinwolf
+ */
 @Service
+@RequiredArgsConstructor
 public class ArticleActionServiceImpl implements ArticleActionService {
-    // TODO 实现功能
+    private final ServiceAuthMediator mediator;
+    private final ArticleCommentMapper commentMapper;
+    private final ArticleFavoriteMapper favoriteMapper;
+    private final ArticleLikeMapper likeMapper;
+
     @Override
     public List<ArticleCommentVo> getArticleCommentVos(Long articleId) {
         return List.of();
+    public PageResult<ArticleCommentVo> getArticleCommentVos(ArticleCommentPageDto dto) {
     }
 
     @Override
-    public List<ArticleCommentVo> postComment(ArticleCommentDto dto) {
-        return List.of();
+    public PageResult<ArticleCommentVo> postComment(ArticleCommentDto dto) {
+        return null;
     }
 
     @Override
-    public List<ArticleCommentVo> deleteComment(Long commentId) {
-        return List.of();
+    public PageResult<ArticleCommentVo> deleteComment(Long commentId) {
+        return null;
     }
 
     @Override
