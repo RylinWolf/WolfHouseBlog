b81b694fc2b3482cf1658b31ac00671d7208ffda
RylinWolf <linexwolf@icloud.com>
2025-08-25 14:49:42 +0800

    feat: 添加常量字段

--- src/main/java/com/wolfhouse/wolfhouseblog/common/utils/verify/VerifyConstant.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/utils/verify/VerifyConstant.java
@@ -6,4 +6,5 @@ package com.wolfhouse.wolfhouseblog.common.utils.verify;
 public class VerifyConstant {
     public static final String VERIFY_FAILED = "字段验证未通过";
     public static final String NOT_ALL_BLANK = "字段不得全为空";
+    public static final String NOT_ANY_BLANK = "字段不得为空";
 }
