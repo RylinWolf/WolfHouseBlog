2b35c93a70e8f9775ec2ff63007a38788ef2a715
RylinWolf <linexwolf@icloud.com>
2025-08-25 14:51:07 +0800

    fix: 文章评论 Vo 对象中，使用回复 ID 代替子评论属性。
    feat: 文章评论 Vo 对象中，新增评论时间属性

--- src/main/java/com/wolfhouse/wolfhouseblog/pojo/vo/ArticleCommentVo.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/pojo/vo/ArticleCommentVo.java
@@ -2,10 +2,16 @@ package com.wolfhouse.wolfhouseblog.pojo.vo;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
+/**
+ * @author linexsong
+ */
 @Data
 public class ArticleCommentVo {
     private Long id;
     private Long userId;
+    private Long replyId;
     private String content;
-    private ArticleCommentVo[] children;
+    private LocalDateTime commentTime;
 }
