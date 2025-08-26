640fe0d218f7b24afd612c4f1b572c1c6ce5d971
RylinWolf <linexwolf@icloud.com>
2025-08-26 19:04:12 +0800

    feat: 添加常量字段

--- src/main/java/com/wolfhouse/wolfhouseblog/common/http/HttpCodeConstant.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/http/HttpCodeConstant.java
@@ -24,4 +24,5 @@ public class HttpCodeConstant {
     public static final String BANNED = "BANNED";
     public static final String USER_NOT_EXIST = "USER_NOT_EXIST";
     public static final String NO_PERMISSION = "NO_PERMISSION";
+    public static final String UNSUPPORTED_MEDIA_TYPE = "MEDIA_TYPE_NOT_SUPPORT";
 }
