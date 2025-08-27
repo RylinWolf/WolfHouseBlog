6cc1bd056c2cd2654bfad386e13c094f673055a2
Ruixe Wolf <RuixeWolf@outlook.com>
2025-08-27 19:33:33 +0800

    feat: 添加常量字段

--- src/main/java/com/wolfhouse/wolfhouseblog/common/constant/services/ArticleConstant.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/constant/services/ArticleConstant.java
@@ -17,7 +17,7 @@ public class ArticleConstant {
 
     public static final String POST_FAILED = "文章发布失败！";
     public static final String UPDATE_FAILED = "文章修改失败！";
-    public static final String ACCESS_DENIED = "无法获取文章信息";
+    public static final String ACCESS_DENIED = "无法访问该文章！";
     public static final String DELETE_FAILED = "文章删除失败！";
     public static final String UPDATE_PARTITION_FAILED = "文章分区更新失败！";
     public static final String COMMENT_NOT_EXIST = "评论不存在！";
