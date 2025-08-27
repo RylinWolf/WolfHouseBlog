e7ae16610ef1857499626e01b91056ec7fd2ae7f
Ruixe Wolf <RuixeWolf@outlook.com>
2025-08-27 19:37:38 +0800

    feat: 新增评论 ID 验证节点

--- src/main/java/com/wolfhouse/wolfhouseblog/common/utils/verify/impl/nodes/article/ArticleVerifyNode.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/utils/verify/impl/nodes/article/ArticleVerifyNode.java
@@ -10,12 +10,20 @@ public class ArticleVerifyNode {
     public static final PrimaryVerifyNode PRIMARY = new PrimaryVerifyNode();
     public static final TitleVerifyNode TITLE = new TitleVerifyNode();
     private static IdReachableVerifyNode ID;
+    private static CommentIdVerifyNode COMMENT_ID;
 
     public static IdReachableVerifyNode id(ServiceAuthMediator mediator) {
         if (ID == null) {
             ID = new IdReachableVerifyNode(mediator);
         }
         return ID;
+    }
+
+    public static CommentIdVerifyNode commentId(ServiceAuthMediator mediator) {
+        if (COMMENT_ID == null) {
+            COMMENT_ID = new CommentIdVerifyNode(mediator);
+        }
+        return COMMENT_ID;
     }
 
     public static ContentVerifyNode content(String t, Boolean allowNull) {
