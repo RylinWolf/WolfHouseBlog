164d820cfd436f56d166946a8544bbe201799b68
RylinWolf <linexwolf@icloud.com>
2025-09-01 19:01:58 +0800

    feat: 添加常量字段

--- src/main/java/com/wolfhouse/wolfhouseblog/common/constant/services/FavoritesConstant.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/constant/services/FavoritesConstant.java
@@ -6,4 +6,5 @@ package com.wolfhouse.wolfhouseblog.common.constant.services;
 public class FavoritesConstant {
     public static final String TITLE_HAS_EXIST = "收藏夹已存在！";
     public static final String NOT_EXIST = "收藏夹不存在！";
+    public static final String ADD_FAILED = "收藏夹新建失败！";
 }
