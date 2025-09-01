17997648856c4045b5f5f911d622ab37d2e7f3e1
RylinWolf <linexwolf@icloud.com>
2025-09-01 18:18:04 +0800

    feat: 新增常量字段

---
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/constant/services/FavoritesConstant.java
@@ -0,0 +1,9 @@
+package com.wolfhouse.wolfhouseblog.common.constant.services;
+
+/**
+ * @author linexsong
+ */
+public class FavoritesConstant {
+    public static final String TITLE_HAS_EXIST = "收藏夹已存在！";
+    public static final String NOT_EXIST = "收藏夹不存在！";
+}
