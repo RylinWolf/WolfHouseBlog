550d3b79afd6be7e06e4675e6b903c4a33c6a99a
RylinWolf <linexwolf@icloud.com>
2025-09-04 14:49:53 +0800

    feat: 新增文件服务接口及 Oss 文件服务接口实现类

---
+++ src/main/java/com/wolfhouse/wolfhouseblog/service/impl/OssFileServiceImpl.java
@@ -0,0 +1,16 @@
+package com.wolfhouse.wolfhouseblog.service.impl;
+
+import com.wolfhouse.wolfhouseblog.service.FileService;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Oss 文件服务实现类
+ *
+ * @author linexsong
+ */
+public class OssFileServiceImpl implements FileService {
+    @Override
+    public String uploadAvatar(MultipartFile file) {
+        return "";
+    }
+}
