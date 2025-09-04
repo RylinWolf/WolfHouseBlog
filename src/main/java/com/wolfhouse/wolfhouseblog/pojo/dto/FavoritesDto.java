a08f8dec09904eac771525a40fd75f706abd51d1
RylinWolf <linexwolf@icloud.com>
2025-09-04 11:25:51 +0800

    feat: 为收藏 Dto 新增全参、无参构造方法

--- src/main/java/com/wolfhouse/wolfhouseblog/pojo/dto/FavoritesDto.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/pojo/dto/FavoritesDto.java
@@ -2,12 +2,16 @@ package com.wolfhouse.wolfhouseblog.pojo.dto;
 
 import com.wolfhouse.wolfhouseblog.common.enums.DefaultEnum;
 import com.wolfhouse.wolfhouseblog.common.enums.VisibilityEnum;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author linexsong
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FavoritesDto {
     private String title;
     private VisibilityEnum visibility;
