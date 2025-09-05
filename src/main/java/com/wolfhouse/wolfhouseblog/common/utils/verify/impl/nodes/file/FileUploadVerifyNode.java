04d1cd82a594f3c023547c85f052ec0851872427
RylinWolf <linexwolf@icloud.com>
2025-09-05 18:50:10 +0800

    feat: 新增文件上传验证节点统一类

---
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/utils/verify/impl/nodes/file/FileUploadVerifyNode.java
@@ -0,0 +1,17 @@
+package com.wolfhouse.wolfhouseblog.common.utils.verify.impl.nodes.file;
+
+import com.wolfhouse.wolfhouseblog.service.OssChunkFileService;
+
+/**
+ * @author linexsong
+ */
+public class FileUploadVerifyNode {
+    private static UploadIdExistVerifyNode UPLOAD_ID_EXIST;
+
+    public static UploadIdExistVerifyNode uploadIdExist(OssChunkFileService service) {
+        if (UPLOAD_ID_EXIST == null) {
+            UPLOAD_ID_EXIST = new UploadIdExistVerifyNode(service);
+        }
+        return UPLOAD_ID_EXIST;
+    }
+}
