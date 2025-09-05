6afd464a73d03e2d7cfdfe4e9b3f3bf21bbd7399
RylinWolf <linexwolf@icloud.com>
2025-09-05 13:32:49 +0800

    feat: 新增文件上传所需 Dto、Vo

---
+++ src/main/java/com/wolfhouse/wolfhouseblog/pojo/dto/file/ChunkFilePermitDto.java
@@ -0,0 +1,22 @@
+package com.wolfhouse.wolfhouseblog.pojo.dto.file;
+
+import lombok.Data;
+
+/**
+ * 分片上传请求 Dto
+ *
+ * @author linexsong
+ */
+@Data
+public class ChunkFilePermitDto {
+    /** 分片数量 */
+    private Integer chunkNumber;
+    /** 分片大小 */
+    private Integer chunkSize;
+    /** 文件名 */
+    private String filename;
+    /** 文件类型 */
+    private String type;
+    /** 文件大小 */
+    private Long size;
+}
---
+++ src/main/java/com/wolfhouse/wolfhouseblog/pojo/vo/file/FileUploadResultVo.java
@@ -0,0 +1,7 @@
+package com.wolfhouse.wolfhouseblog.pojo.vo.file;
+
+/**
+ * @author linexsong
+ */
+public class FileUploadResultVo {
+}
