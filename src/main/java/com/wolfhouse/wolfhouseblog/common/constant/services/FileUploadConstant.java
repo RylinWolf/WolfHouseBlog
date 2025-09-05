2486da6c606b0c4c2fa26b644eb5800a704747df
RylinWolf <linexwolf@icloud.com>
2025-09-05 18:21:56 +0800

    feat: 新增文件相关常量

---
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/constant/services/FileUploadConstant.java
@@ -0,0 +1,8 @@
+package com.wolfhouse.wolfhouseblog.common.constant.services;
+
+/**
+ * @author linexsong
+ */
+public class FileUploadConstant {
+    public static final String INIT_FAILED = "文件初始化失败！";
+}
