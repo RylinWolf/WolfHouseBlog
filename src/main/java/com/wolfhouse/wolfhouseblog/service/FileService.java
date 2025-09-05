84325efb53491f0a68d810067bba5871caa4ae41
RylinWolf <linexwolf@icloud.com>
2025-09-05 13:33:25 +0800

    feat: 新增头像上传相关业务方法及其空实现

--- src/main/java/com/wolfhouse/wolfhouseblog/service/FileService.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/service/FileService.java
@@ -1,6 +1,10 @@
 package com.wolfhouse.wolfhouseblog.service;
 
-import org.springframework.web.multipart.MultipartFile;
+import com.wolfhouse.wolfhouseblog.pojo.dto.file.ChunkFilePermitDto;
+import com.wolfhouse.wolfhouseblog.pojo.vo.file.ChunkFilePermitVo;
+import com.wolfhouse.wolfhouseblog.pojo.vo.file.FileUploadResultVo;
+
+import java.io.InputStream;
 
 /**
  * @author linexsong
@@ -9,8 +13,25 @@ public interface FileService {
     /**
      * 上传用户头像文件
      *
-     * @param file 要上传的头像文件
+     * @param uploadId 上传 Id
+     * @param ins      要上传的头像文件输入流
      * @return 上传成功后的头像文件访问URL
      */
-    String uploadAvatar(MultipartFile file);
+    FileUploadResultVo uploadAvatar(Long uploadId, InputStream ins);
+
+    /**
+     * 开始用户头像文件分块上传。
+     *
+     * @param dto 分块上传的准许信息数据传输对象，包含分块编号等上传必要信息。
+     * @return 分块文件上传准许信息视图对象，包含上传所需的相关信息。
+     */
+    ChunkFilePermitVo startUploadAvatar(ChunkFilePermitDto dto);
+
+    /**
+     * 停止用户头像文件的上传操作。
+     *
+     * @param uploadId 上传任务的唯一标识符
+     * @return 包含上传结果的文件上传结果视图对象
+     */
+    FileUploadResultVo stopUploadAvatar(Long uploadId);
 }
--- src/main/java/com/wolfhouse/wolfhouseblog/service/impl/OssFileServiceImpl.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/service/impl/OssFileServiceImpl.java
@@ -1,16 +1,32 @@
 package com.wolfhouse.wolfhouseblog.service.impl;
 
+import com.wolfhouse.wolfhouseblog.pojo.dto.file.ChunkFilePermitDto;
+import com.wolfhouse.wolfhouseblog.pojo.vo.file.ChunkFilePermitVo;
+import com.wolfhouse.wolfhouseblog.pojo.vo.file.FileUploadResultVo;
 import com.wolfhouse.wolfhouseblog.service.FileService;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
 
 /**
  * Oss 文件服务实现类
  *
  * @author linexsong
  */
+@Service
 public class OssFileServiceImpl implements FileService {
     @Override
-    public String uploadAvatar(MultipartFile file) {
-        return "";
+    public FileUploadResultVo uploadAvatar(Long uploadId, InputStream ins) {
+        return null;
+    }
+
+    @Override
+    public ChunkFilePermitVo startUploadAvatar(ChunkFilePermitDto dto) {
+        return null;
+    }
+
+    @Override
+    public FileUploadResultVo stopUploadAvatar(Long uploadId) {
+        return null;
     }
 }
