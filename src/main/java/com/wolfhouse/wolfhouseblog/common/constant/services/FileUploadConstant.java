dc9bbdf83515d6996b487e5cafe25075c29e0090
RylinWolf <linexwolf@icloud.com>
2025-09-05 18:38:28 +0800

    feat: 添加常量字段

--- src/main/java/com/wolfhouse/wolfhouseblog/common/constant/services/FileUploadConstant.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/constant/services/FileUploadConstant.java
@@ -5,4 +5,5 @@ package com.wolfhouse.wolfhouseblog.common.constant.services;
  */
 public class FileUploadConstant {
     public static final String INIT_FAILED = "文件初始化失败！";
+    public static final String FILE_META_SAVE_FAILED = "文件元信息保存失败！";
 }
