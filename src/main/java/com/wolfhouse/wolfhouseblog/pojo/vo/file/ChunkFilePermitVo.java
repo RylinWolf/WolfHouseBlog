56e3e971f994a172070f0285935647839529d224
RylinWolf <linexwolf@icloud.com>
2025-09-05 16:46:48 +0800

    feat: 完善 OSS 相关 POJO

--- src/main/java/com/wolfhouse/wolfhouseblog/pojo/vo/file/ChunkFilePermitVo.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/pojo/vo/file/ChunkFilePermitVo.java
@@ -1,7 +1,16 @@
 package com.wolfhouse.wolfhouseblog.pojo.vo.file;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author linexsong
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChunkFilePermitVo {
+    private String uploadId;
+    private Integer statusCode;
 }
--- src/main/java/com/wolfhouse/wolfhouseblog/pojo/vo/file/FileUploadResultVo.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/pojo/vo/file/FileUploadResultVo.java
@@ -1,7 +1,20 @@
 package com.wolfhouse.wolfhouseblog.pojo.vo.file;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author linexsong
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FileUploadResultVo {
+    /** 上传完成的分片标记 */
+    private String eTag;
+    /** 上传完成的分片号 */
+    private Long partNumber;
+    /** 上传完成的状态码 */
+    private Integer statusCode;
 }
