988468a76265df40430ea407b69994c880cf6d71
RylinWolf <linexwolf@icloud.com>
2025-09-14 16:50:02 +0800

    feat: 新增 Url 匹配工具类，提供公共 Url 匹配方法

---
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/utils/UrlMatchUtil.java
@@ -0,0 +1,59 @@
+package com.wolfhouse.wolfhouseblog.common.utils;
+
+import com.wolfhouse.wolfhouseblog.common.constant.SecurityConstant;
+import org.springframework.http.server.PathContainer;
+import org.springframework.web.util.pattern.PathPattern;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author linexsong
+ */
+public class UrlMatchUtil {
+    @SuppressWarnings("FieldMayBeFinal")
+    private List<PathPattern> publicUrlList;
+
+    private UrlMatchUtil() {
+        this.publicUrlList = Arrays.stream(SecurityConstant.PUBLIC_URLS)
+                                   .map(PathPatternParser.defaultInstance::parse)
+                                   .toList();
+    }
+
+    private static class SingletonHolder {
+        private static UrlMatchUtil INSTANCE;
+
+        private static UrlMatchUtil getInstance() {
+            if (INSTANCE == null) {
+                INSTANCE = new UrlMatchUtil();
+            }
+            return INSTANCE;
+        }
+    }
+
+    public static UrlMatchUtil instance() {
+        return SingletonHolder.getInstance();
+    }
+
+    public List<PathPattern> add(String... urls) {
+        this.publicUrlList.addAll(
+            Arrays.stream(urls)
+                  .map(PathPatternParser.defaultInstance::parse)
+                  .toList());
+        return this.publicUrlList;
+    }
+
+    public List<PathPattern> remove(String... urls) {
+        this.publicUrlList.removeAll(
+            Arrays.stream(urls)
+                  .map(PathPatternParser.defaultInstance::parse)
+                  .toList());
+        return this.publicUrlList;
+    }
+
+    public Boolean isPublic(String url) {
+        return this.publicUrlList.stream()
+                                 .anyMatch(p -> p.matches(PathContainer.parsePath(url)));
+    }
+}
