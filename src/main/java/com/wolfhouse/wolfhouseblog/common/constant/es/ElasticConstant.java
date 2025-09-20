ae9d55eb32227369db05c7882ff9452c23077cbe
RylinWolf <linexwolf@icloud.com>
2025-09-21 07:35:42 +0800

    feat: 新增常量字段

---
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/constant/es/ElasticConstant.java
@@ -0,0 +1,8 @@
+package com.wolfhouse.wolfhouseblog.common.constant.es;
+
+/**
+ * @author linexsong
+ */
+public class ElasticConstant {
+    public static final String ARTICLE_INDEX = "article";
+}
