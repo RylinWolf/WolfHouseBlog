90a237bd57da63f9c96698e20aa08394d8905697
RylinWolf <linexwolf@icloud.com>
2025-09-20 23:23:56 +0800

    feat: 新增常量字段

--- src/main/java/com/wolfhouse/wolfhouseblog/common/constant/redis/UserRedisConstant.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/constant/redis/UserRedisConstant.java
@@ -8,9 +8,11 @@ public class UserRedisConstant {
     public static final String BASE = RedisConstant.format(false, USER);
 
     /** 权限字段 */
-    public static final String AUTHORITIES = RedisConstant.format(false, BASE, "authorities");
+    public static final String AUTHORITIES =
+        RedisConstant.format(false, BASE, "authorities") + RedisConstant.SEPARATOR + "%d";
 
     /** Token */
-    public static final String TOKEN = RedisConstant.format(false, BASE, "token");
+    public static final String TOKEN =
+        RedisConstant.format(false, BASE, "token") + RedisConstant.SEPARATOR + "%s";
 
 }
