6e97b0ffd0255f60765c5a5ee7bcd0fd2fb7dd72
RylinWolf <linexwolf@icloud.com>
2025-09-20 23:25:40 +0800

    feat: 新增角色相关 Redis 服务类

---
+++ src/main/java/com/wolfhouse/wolfhouseblog/redis/RoleRedisService.java
@@ -0,0 +1,64 @@
+package com.wolfhouse.wolfhouseblog.redis;
+
+import com.wolfhouse.wolfhouseblog.common.constant.redis.UserRedisConstant;
+import com.wolfhouse.wolfhouseblog.pojo.domain.Authority;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author linexsong
+ */
+@Component
+public class RoleRedisService {
+    /** 基础过期时间 */
+    public static final Long BASE_TIME_OUT = 24 * 60L;
+
+    private final ValueOperations<String, Object> valueOps;
+
+    @Autowired
+    public RoleRedisService(RedisTemplate<String, Object> redisTemplate) {
+        this.valueOps = redisTemplate.opsForValue();
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Authority> getAuthorities(final Long userId) {
+        Object o = valueOps.get(UserRedisConstant.AUTHORITIES.formatted(userId));
+        return o == null ? null : (List<Authority>) o;
+    }
+
+    public void saveAuthorities(final Long userId, final List<Authority> authorities) {
+        String key = UserRedisConstant.AUTHORITIES.formatted(userId);
+        valueOps.set(key, authorities, randTimeout(), TimeUnit.MINUTES);
+    }
+
+    /**
+     * 根据 token 获取缓存的 userId
+     *
+     * @param token token
+     * @return token 对应的 userId
+     */
+    public Long findToken(final String token) {
+        Object o = valueOps.get(UserRedisConstant.TOKEN.formatted(token));
+        return o != null ? Long.parseLong(o.toString()) : null;
+    }
+
+    public void saveTokenCache(final String token, final String userId) {
+        valueOps.set(UserRedisConstant.TOKEN.formatted(token), userId, randTimeout(), TimeUnit.MINUTES);
+    }
+
+    /**
+     * 获取随机时间延迟
+     * 范围波动：30
+     *
+     * @return Long 随机的时间延迟
+     */
+    private Long randTimeout() {
+        return BASE_TIME_OUT + new Random().nextLong(30);
+    }
+}
