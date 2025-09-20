60f06544a8ea3c27375b3f499cf1acca40c8d674
RylinWolf <linexwolf@icloud.com>
2025-09-20 17:56:56 +0800

    feat: 新增 Redis 配置类

---
+++ src/main/java/com/wolfhouse/wolfhouseblog/config/RedisConfig.java
@@ -0,0 +1,22 @@
+package com.wolfhouse.wolfhouseblog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+/**
+ * @author linexsong
+ */
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        template.setValueSerializer(RedisSerializer.json());
+        template.setKeySerializer(RedisSerializer.string());
+        return template;
+    }
+}
