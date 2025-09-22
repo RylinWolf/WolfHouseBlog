59af7224658cfeb7f41cc3bd642af3469186e5e7
RylinWolf <linexwolf@icloud.com>
2025-09-23 01:37:32 +0800

    feat: 为 ES MQ 监听器添加更新、删除 方法

--- src/main/java/com/wolfhouse/wolfhouseblog/mq/listener/ArticleEsListener.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/mq/listener/ArticleEsListener.java
@@ -1,8 +1,10 @@
 package com.wolfhouse.wolfhouseblog.mq.listener;
 
 import com.wolfhouse.wolfhouseblog.common.constant.mq.MqArticleEsConstant;
+import com.wolfhouse.wolfhouseblog.common.exceptions.ServiceException;
 import com.wolfhouse.wolfhouseblog.es.ArticleElasticServiceImpl;
 import com.wolfhouse.wolfhouseblog.pojo.domain.Article;
+import com.wolfhouse.wolfhouseblog.pojo.dto.ArticleUpdateDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.ExchangeTypes;
 import org.springframework.amqp.rabbit.annotation.Exchange;
@@ -30,5 +32,36 @@ public class ArticleEsListener {
     public void post(Article article) {
         articleService.saveOne(article);
     }
-    
+
+    @RabbitListener(bindings = @QueueBinding(
+        value = @Queue(name = MqArticleEsConstant.UPDATE_QUEUE),
+        exchange = @Exchange(
+            name = MqArticleEsConstant.UPDATE_EXCHANGE,
+            type = ExchangeTypes.TOPIC
+        ),
+        key = {MqArticleEsConstant.UPDATE_KEY}
+    ))
+    public void update(ArticleUpdateDto dto) {
+        try {
+            articleService.update(dto);
+        } catch (Exception e) {
+            throw new ServiceException(e.getMessage(), e);
+        }
+    }
+
+    @RabbitListener(bindings = @QueueBinding(
+        value = @Queue(name = MqArticleEsConstant.DELETE_QUEUE),
+        exchange = @Exchange(
+            name = MqArticleEsConstant.DELETE_EXCHANGE,
+            type = ExchangeTypes.TOPIC
+        ),
+        key = {MqArticleEsConstant.DELETE_KEY}
+    ))
+    public void delete(Long id) {
+        try {
+            articleService.deleteById(id);
+        } catch (Exception e) {
+            throw new ServiceException(e.getMessage(), e);
+        }
+    }
 }
