d99da9fdc40426d82ef0c84cdc12123d801d0477
RylinWolf <linexwolf@icloud.com>
2025-09-22 16:16:54 +0800

    perf: 优化文章 ES 服务，进行更多配置

--- src/main/java/com/wolfhouse/wolfhouseblog/es/ArticleElasticService.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/es/ArticleElasticService.java
@@ -2,6 +2,7 @@ package com.wolfhouse.wolfhouseblog.es;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch.core.BulkRequest;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
 import co.elastic.clients.elasticsearch.core.search.Hit;
 import co.elastic.clients.elasticsearch.core.search.TotalHits;
@@ -9,6 +10,8 @@ import com.mybatisflex.core.paginate.Page;
 import com.wolfhouse.wolfhouseblog.common.constant.es.ElasticConstant;
 import com.wolfhouse.wolfhouseblog.common.exceptions.ServiceException;
 import com.wolfhouse.wolfhouseblog.common.utils.page.PageResult;
+import com.wolfhouse.wolfhouseblog.config.ElasticSearchConfig;
+import com.wolfhouse.wolfhouseblog.pojo.domain.Article;
 import com.wolfhouse.wolfhouseblog.pojo.dto.ArticleQueryPageDto;
 import com.wolfhouse.wolfhouseblog.pojo.vo.ArticleBriefVo;
 import jakarta.annotation.PostConstruct;
@@ -27,6 +30,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ArticleElasticService {
     private final ElasticsearchClient client;
+    private final ElasticSearchConfig config;
 
     @PostConstruct
     public void init() throws IOException {
@@ -46,6 +50,14 @@ public class ArticleElasticService {
             client.indices()
                   .create(r -> {
                       r.index(ElasticConstant.ARTICLE_INDEX);
+                      r.mappings(f -> {
+                          f.properties(ElasticConstant.ARTICLE_MAPPING);
+                          return f;
+                      });
+                      r.settings(s -> {
+                          s.maxResultWindow(config.maxResultWindow);
+                          return s;
+                      });
                       return r;
                   });
             log.info("索引库 {} 初始化完成", ElasticConstant.ARTICLE_INDEX);
@@ -56,38 +68,20 @@ public class ArticleElasticService {
     }
 
     public PageResult<ArticleBriefVo> getBriefVoList(ArticleQueryPageDto dto) throws IOException {
-        Integer pageSize = Integer.valueOf(dto.getPageSize()
-                                              .toString());
+        long pageSize = dto.getPageSize();
         SearchResponse<ArticleBriefVo> response = client.search(r -> {
             r.index(ElasticConstant.ARTICLE_INDEX);
             // 分页参数
-            r.size(pageSize);
-            r.from((int) (dto.getPageSize() * (dto.getPageNumber() - 1)));
+            buildPagination(r, (int) (dto.getPageSize() * (dto.getPageNumber() - 1)), (int) pageSize);
             // 获取总条数
-            r.trackTotalHits(b -> {
-                b.enabled(true);
-                return b;
-            });
             return r;
         }, ArticleBriefVo.class);
-        List<Hit<ArticleBriefVo>> hits = response.hits()
-                                                 .hits();
-
-        Page<ArticleBriefVo> page = new Page<>();
-        TotalHits total = response.hits()
-                                  .total();
-        page.setTotalRow(total == null ? 0 : total.value());
-        page.setRecords(hits.stream()
-                            .map(Hit::source)
-                            .toList());
-        page.setPageNumber(dto.getPageNumber());
-        page.setTotalPage(page.getTotalRow() / page.getPageSize());
-        return PageResult.of(page);
+        return PageResult.of(toPage(response, dto.getPageNumber(), pageSize));
     }
 
-    public void saveVoList(List<ArticleBriefVo> voList) throws IOException {
-        int size = voList.size();
-        final int batch = 100;
+    public void saveBatch(List<Article> articles) throws IOException {
+        int size = articles.size();
+        final int batch = 200;
         int index = 0;
         int round = size / batch;
 
@@ -104,10 +98,10 @@ public class ArticleElasticService {
                     // 构建
                     op.index(idx -> {
                         idx.index(ElasticConstant.ARTICLE_INDEX);
-                        idx.id(voList.get(nowIndex)
-                                     .getId()
-                                     .toString());
-                        idx.document(voList.get(nowIndex));
+                        idx.id(articles.get(nowIndex)
+                                       .getId()
+                                       .toString());
+                        idx.document(articles.get(nowIndex));
                         return idx;
                     });
                     return op;
@@ -117,5 +111,49 @@ public class ArticleElasticService {
 
             client.bulk(builder.build());
         }
+    }
+
+    public void saveOne(Article article) {
+        try {
+            client.create(c -> {
+                c.index(ElasticConstant.ARTICLE_INDEX);
+                c.id(article.getId()
+                            .toString());
+                c.document(article);
+                return c;
+            });
+        } catch (IOException e) {
+            log.error("存储文章至 ES 失败！Article: {}", article, e);
+            throw new ServiceException(e.getMessage(), e);
+        }
+    }
+
+    private void buildPagination(SearchRequest.Builder builder, int from, int size) {
+        builder.trackTotalHits(b -> {
+            b.enabled(true);
+            return b;
+        });
+        builder.from(from);
+        builder.size(size);
+    }
+
+    private <T> Page<T> toPage(SearchResponse<T> response, long pageNum, long pageSize) {
+        Page<T> page = new Page<>();
+        var hits = response.hits()
+                           .hits();
+        // 记录
+        page.setRecords(hits.stream()
+                            .map(Hit::source)
+                            .toList());
+
+        // 总行数
+        TotalHits total = response.hits()
+                                  .total();
+
+        page.setTotalRow(total == null ? 0 : total.value());
+        page.setPageNumber(pageNum);
+        page.setPageSize(pageSize);
+        page.setTotalPage(page.getTotalRow() / page.getPageSize());
+        return page;
     }
 }
