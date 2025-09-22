01f132e71c21bc6dae7e8186597f7f2bcec999b6
RylinWolf <linexwolf@icloud.com>
2025-09-23 01:38:37 +0800

    feat: 为 ES MQ 服务类添加更新、删除 方法

--- src/main/java/com/wolfhouse/wolfhouseblog/mq/service/MqEsService.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/mq/service/MqEsService.java
@@ -16,10 +16,31 @@ public class MqEsService {
 
     /**
      * 发布文章
+     * 将文章对象发送到消息队列用于ES索引创建
      *
      * @param article 文章对象
      */
     public void postArticle(Article article) {
         template.convertAndSend(MqArticleEsConstant.POST_EXCHANGE, MqArticleEsConstant.POST_QUEUE, article);
+    }
+
+    /**
+     * 更新文章
+     * 将文章对象发送到消息队列用于ES索引更新
+     *
+     * @param article 文章对象
+     */
+    public void updateArticle(Article article) {
+        template.convertAndSend(MqArticleEsConstant.UPDATE_EXCHANGE, MqArticleEsConstant.UPDATE_QUEUE, article);
+    }
+
+    /**
+     * 删除文章
+     * 将文章ID发送到消息队列用于ES索引删除
+     *
+     * @param id 文章ID
+     */
+    public void deleteArticle(Long id) {
+        template.convertAndSend(MqArticleEsConstant.DELETE_EXCHANGE, MqArticleEsConstant.DELETE_QUEUE, id);
     }
 }
