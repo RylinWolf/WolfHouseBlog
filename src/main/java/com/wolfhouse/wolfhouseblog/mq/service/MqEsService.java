43c2dc417281f478db157d886689d97e373a65b0
RylinWolf <linexwolf@icloud.com>
2025-09-23 01:48:09 +0800

    fix: ES MQ 服务类的文章更新方法现在会传输正确的参数

--- src/main/java/com/wolfhouse/wolfhouseblog/mq/service/MqEsService.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/mq/service/MqEsService.java
@@ -2,6 +2,7 @@ package com.wolfhouse.wolfhouseblog.mq.service;
 
 import com.wolfhouse.wolfhouseblog.common.constant.mq.MqArticleEsConstant;
 import com.wolfhouse.wolfhouseblog.pojo.domain.Article;
+import com.wolfhouse.wolfhouseblog.pojo.dto.ArticleUpdateDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
@@ -28,10 +29,10 @@ public class MqEsService {
      * 更新文章
      * 将文章对象发送到消息队列用于ES索引更新
      *
-     * @param article 文章对象
+     * @param dto 文章更新对象
      */
-    public void updateArticle(Article article) {
-        template.convertAndSend(MqArticleEsConstant.UPDATE_EXCHANGE, MqArticleEsConstant.UPDATE_QUEUE, article);
+    public void updateArticle(ArticleUpdateDto dto) {
+        template.convertAndSend(MqArticleEsConstant.UPDATE_EXCHANGE, MqArticleEsConstant.UPDATE_QUEUE, dto);
     }
 
     /**
