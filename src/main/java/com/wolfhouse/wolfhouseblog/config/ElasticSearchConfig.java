87eb108c440eb6a2130d264e8add4147bce627c7
RylinWolf <linexwolf@icloud.com>
2025-09-22 16:17:41 +0800

    feat: 通过新的认证方式连接 ES 客户端

--- src/main/java/com/wolfhouse/wolfhouseblog/config/ElasticSearchConfig.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/config/ElasticSearchConfig.java
@@ -5,9 +5,11 @@ import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
-import org.apache.http.Header;
 import org.apache.http.HttpHost;
-import org.apache.http.message.BasicHeader;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -20,14 +22,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties(prefix = "custom.elasticsearch")
 public class ElasticSearchConfig {
-    public final String API_KEY = System.getenv("ELASTIC_SEARCH_API_KEY");
+    public static final String API_KEY = System.getenv("ELASTIC_SEARCH_API_KEY");
+    public static final String USERNAME = System.getenv("ELASTIC_SEARCH_USERNAME");
+    public static final String PASSWORD = System.getenv("ELASTIC_SEARCH_PASSWORD");
     public String host;
+    public Integer maxResultWindow;
+
 
     @Bean(destroyMethod = "close")
     public ElasticsearchClient elasticsearchClient(ObjectMapper defaultObjectMapper) {
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(USERNAME, PASSWORD));
         RestClient restClient = RestClient.builder(HttpHost.create(host))
-                                          .setDefaultHeaders(new Header[]{new BasicHeader("Authorization",
-                                                                                          "ApiKey " + API_KEY)})
+                                          .setHttpClientConfigCallback(httpClientBuilder ->
+                                                                           httpClientBuilder.setDefaultCredentialsProvider(
+                                                                               credentialsProvider))
                                           .build();
         return new ElasticsearchClient(new RestClientTransport(restClient,
                                                                new JacksonJsonpMapper(defaultObjectMapper)));
