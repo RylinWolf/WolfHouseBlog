aea94b24b921e79c43b5769253d6e72371513446
RylinWolf <linexwolf@icloud.com>
2025-09-27 18:33:05 +0800

    feat: 新增 ES 常量类

---
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/constant/es/EsConstant.java
@@ -0,0 +1,10 @@
+package com.wolfhouse.wolfhouseblog.common.constant.es;
+
+/**
+ * @author linexsong
+ */
+public class EsConstant {
+
+    public static final String UPDATED = "updated";
+    public static final String NOOP = "noop";
+}
