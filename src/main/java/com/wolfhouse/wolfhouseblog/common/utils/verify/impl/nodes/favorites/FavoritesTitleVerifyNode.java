9bcdcee5f525f0e41292fed1e6391d789691a100
RylinWolf <linexwolf@icloud.com>
2025-10-09 17:19:36 +0800

    fix: 修复了错误的收藏夹标题验证逻辑

--- src/main/java/com/wolfhouse/wolfhouseblog/common/utils/verify/impl/nodes/favorites/FavoritesTitleVerifyNode.java
+++ src/main/java/com/wolfhouse/wolfhouseblog/common/utils/verify/impl/nodes/favorites/FavoritesTitleVerifyNode.java
@@ -1,8 +1,11 @@
 package com.wolfhouse.wolfhouseblog.common.utils.verify.impl.nodes.favorites;
 
+import com.wolfhouse.wolfhouseblog.common.utils.verify.VerifyException;
 import com.wolfhouse.wolfhouseblog.common.utils.verify.impl.BaseVerifyNode;
 import com.wolfhouse.wolfhouseblog.common.utils.verify.impl.nodes.commons.StringVerifyNode;
 import com.wolfhouse.wolfhouseblog.service.mediator.ServiceAuthMediator;
+
+import static com.wolfhouse.wolfhouseblog.pojo.domain.table.FavoritesTableDef.FAVORITES;
 
 /**
  * @author linexsong
@@ -12,6 +15,7 @@ public class FavoritesTitleVerifyNode extends BaseVerifyNode<String> {
 
     public FavoritesTitleVerifyNode(ServiceAuthMediator mediator) {
         this.mediator = mediator;
+        this.customException = new VerifyException(FAVORITES.TITLE.getName());
     }
 
     @Override
@@ -23,7 +27,7 @@ public class FavoritesTitleVerifyNode extends BaseVerifyNode<String> {
             return new StringVerifyNode(1L, 10L, allowNull)
                        .target(t)
                        .verify() &&
-                   mediator.isFavoritesTitleExist(t);
+                   !mediator.isFavoritesTitleExist(t);
         } catch (Exception e) {
             return false;
         }
